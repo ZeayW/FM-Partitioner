fabcb721e0f7dca25e6290a764379a0237cacfb0
ZeayW <16307110042@fudan.edu.cn>
2022-02-06 12:26:24 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -483,7 +483,7 @@ void FMAlgorithm(){
         if (afccnt && bfccnt){
             Cell * a = findMaxGain(0), * b = findMaxGain(1);
             if (a->gain >= b->gain) {
-                n = 0;
+                int n = 0;
                 while(abs(acsz-bcsz-2*a->size) >= error && a->to->next!=NULL && n++ < 5){
                     a = vc[a->to->next->id];
                 }
